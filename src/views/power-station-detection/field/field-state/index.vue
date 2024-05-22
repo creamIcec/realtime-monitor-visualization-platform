da35a0639dcbf8f5a06ecf7cf1e851c05c308040
Iriseplosc <42510470+creamIcec@users.noreply.github.com>
2024-05-22 12:41:04 +0800

    fully merged pages

--- src/views/power-station-detection/field/field-state/index.vue
+++ src/views/power-station-detection/field/field-state/index.vue
@@ -1,165 +1,114 @@
-<script lang="ts" setup>
-import { AreaBar } from '@/components/AreaBar';
-import { RangePicker, Statistic } from 'ant-design-vue';
-
-import { Button, Card, Col, Row } from 'ant-design-vue';
-import { computed } from 'vue';
-
-import { DataTuple } from "@/types/extra";
-import { valueType } from 'ant-design-vue/lib/statistic/utils';
-
-import MapProvider from './components/map-provider.vue';
-import Statistics from "./components/statistics.vue";
-
-const dateFormat = "YYYY/MM/DD";
-const props = defineProps({
-  title: {
-    type: String,
-    default: ''
-  },
-  dataTitles: {
-    type: Array<String>,
-    default: [''],
-    required: false   //temp
-  },
-  dataContents: {
-    type: Array<String | Number>,
-    default: [''],
-    required: false   //temp
-  }
-});
-
-const tempData = {
-  dataTitles: [
-    "累计充电度数",
-    "累计充电次数",
-    "累计充电时长",
-    "累计充电金额"
-  ],
-  dataContents: [
-    "1650024",
-    "63882",
-    "82537",
-    "1300523"
-  ],
-  titles: [
-    "基本数据",
-    "统计图",
-    "快速操作",
-    "来源分布地图"
-  ],
-  fastOperations: [
-    [
-      "导出统计数据",
-      "进入数字孪生",
-    ],
-    [
-      "进入财务管理",
-      "进入场地管理",
-    ],
-    [
-      "进入充电管理",
-      "打开用户手册"
-    ]
-  ]
-};
-
-const contents = computed(() => {
-  const result: DataTuple[] = [];
-  let i = 0;
-  while (i < tempData.dataContents.length) {
-    result.push({ description: tempData.dataTitles[i], value: tempData.dataContents[i] });
-    i++;
-  }
-  return result;
-})
-</script>
 <template>
-  <div class="page-wrapper">
-    <div class="field-state-basic-data">
-      <AreaBar :title-message="tempData.titles[0]" :title="tempData.titles[0]">
-        <RangePicker :format="dateFormat"/>
-      </AreaBar>
-      <div class="statistic-data-container">
-        <Card v-for="tuple in contents">
-          <Statistic :title="tuple.description" :value="tuple.value as valueType" :value-style="{ color: '#3f8600' }"
-            style="margin-right: 50px" />
-        </Card>
+  <div>
+    <a-dropdown>
+      <template #overlay>
+        <a-menu @click="handleMenuClick">
+          <a-menu-item key="Running">运营中的场地</a-menu-item>
+          <a-menu-item key="Stopped">未开放的场地</a-menu-item>
+        </a-menu>
+      </template>
+      <a-button> 场地切换 <DownOutlined /> </a-button>
+    </a-dropdown>
+    <div class="venue-container">
+      <div v-if="selectedType === 'Running'" class="venue-content">
+        <!-- 运营中的场地内容 -->
+        <img src="/resource/img/mock/map/Running.jpg" alt="运营中的场地" />
       </div>
-    </div>
-    <div class="field-state-statistics">
-      <AreaBar :title-message="tempData.titles[1]" :title="tempData.titles[1]"></AreaBar>
-      <div>
-        <Card>
-          <Statistics></Statistics>
-        </Card>
-        <div class="two-elements-wrapper" style="padding: 10px;">
-          <span>选择时间段</span>
-          <RangePicker picker="year"/>
-        </div>
+      <div v-else-if="selectedType === 'Stopped'" class="venue-content">
+        <!-- 未开放的场地内容 -->
+        <img src="/resource/img/mock/map/Stopped.jpg" alt="未开放的场地" />
       </div>
-    </div>
-    <div class="field-state-fast-operation">
-      <AreaBar :title-message="tempData.titles[2]" :title="tempData.titles[2]"></AreaBar>
-      <Card>
-        <Row v-for="rowItem in tempData.fastOperations">
-          <Col v-for="columnItem in rowItem">
-            <Card>
-              <Button size="large">{{ columnItem }}</Button>
-            </Card>
-          </Col>
-        </Row>
-      </Card>
-    </div>
-    <div class="field-state-map">
-      <AreaBar :title-message="tempData.titles[3]" :title="tempData.titles[3]"></AreaBar>
-      <Card>
-          <MapProvider></MapProvider>
-      </Card>
+      <div class="notification-list">
+        <a-list item-layout="horizontal" :data-source="notifications">
+          <template #renderItem="{ item }">
+            <a-list-item>
+              <a-list-item-meta>
+                <template #title>
+                  <a :href="item.link">{{ item.title }}</a>
+                </template>
+                <template #avatar>
+                  <a-avatar :src="item.avatar" />
+                </template>
+              </a-list-item-meta>
+            </a-list-item>
+          </template>
+        </a-list>
+      </div>
     </div>
   </div>
 </template>
-<style scoped>
-.page-wrapper{
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: start;
-  align-items: center;
-  column-gap: 50px;
-  row-gap: 50px;
-  padding: 10px;
-}
-.statistic-data-container{
-  width: 80%;
-  height: fit-content;
-  display: flex;
-  flex-direction: row;
-  column-gap: 20px;
-  justify-content: space-around;
-  padding: 20px;
-}
-.field-state-basic-data{
+
+<script>
+import { defineComponent } from 'vue'
+import { DownOutlined } from '@ant-design/icons-vue'
+import { Dropdown, Menu, Button, List, Avatar } from 'ant-design-vue'
+
+export default defineComponent({
+  name: 'HelloWorld',
+  components: {
+    'a-dropdown': Dropdown,
+    'a-menu': Menu,
+    'a-menu-item': Menu.Item,
+    'a-button': Button,
+    'a-list': List,
+    'a-list-item': List.Item,
+    'a-list-item-meta': List.Item.Meta,
+    'a-avatar': Avatar,
+    DownOutlined
+  },
+  data() {
+    return {
+      selectedType: 'Running', // 默认展示运营中的场地内容
+      notifications: [
+        {
+          title: '严重警告：XX场地即将关闭'
+        },
+        {
+          title: '警告：XX场地使用时间已超过规定时间'
+        },
+        {
+          title: '通知：XX场地维护将于下周一开始，请提前安排'
+        },
+        {
+          title: '通知：XX场地将于本周五进行清洁，请提前安排工作'
+        },
+        {
+          title: '通知：XX场地将于明天进行维修，届时将暂停使用'
+        },
+        {
+          title: '警告：XX场地尚未开放'
+        },
+        {
+          title: '严重警告：XX场地发生火灾'
+        }
+      ]
+    }
+  },
+  methods: {
+    handleMenuClick({ key }) {
+      this.selectedType = key
+    }
+  }
+})
+</script>
+
+<style>
+img {
   width: 100%;
-  height: 200px;
+  max-width: 500px;
+  margin: 10px 0;
 }
-.field-state-fast-operation{
-  width: fit-content;
-  height: 600px;
-}
-.field-state-statistics{
-  width: 600px;
-  height: 600px;
-}
-.field-state-map{
-  width: 300px;
-  height: 600px;
-}
-.two-elements-wrapper{
+
+.venue-container {
   display: flex;
-  flex-direction: row;
-  justify-content: start;
-  align-items: center;
-  column-gap: 10px;
+}
+
+.venue-content {
+  flex: 1;
+  padding-right: 20px;
+}
+
+.notification-list {
+  flex: 1;
 }
 </style>
